23d9e14a72dc75d902f79012de50fabebfad282c
Jason Butwell <jasonbutwell@gmail.com>
2017-02-19 23:28:39 +0000

    Created a Person class to store some data and created the data binding in MainActivity.

--- app/src/main/java/com/jasonbutwell/databindingexample/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/databindingexample/MainActivity.java
@@ -3,7 +3,12 @@ package com.jasonbutwell.databindingexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jasonbutwell.databindingexample.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    // We have this because we enclosed our layout.xml in a <layout> tag
+    ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
