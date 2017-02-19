dc5801142e307159bdab58f927003f125a939997
Jason Butwell <jasonbutwell@gmail.com>
2017-02-19 23:39:26 +0000

    Created the Binding, set up content view with DataBindingUtil, and set the UI fields according to the data in our example Person object by means of a small helper class.

--- app/src/main/java/com/jasonbutwell/databindingexample/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/databindingexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jasonbutwell.databindingexample;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -14,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // create a person to store some name as some example data
+        Person testPerson = new Person("Suzanne","Starshine");
+
+        // Set the binding to point to the layout
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        // call our helper method passing in the person object we created
+        setViewFields( testPerson );
     }
+    
+    private void setViewFields( Person person ) {
+
+        // check to ensure that the object passed in is not null
+        if ( person != null ) {
+            // set the first name of the UI view element to match the persons first name
+            mainBinding.firstName.setText( person.getFirstName() );
+
+            // set the last name of the UI view element to match the persons last name
+            mainBinding.lastName.setText( person.getLastName() );
+        }
+    }
+
 }
